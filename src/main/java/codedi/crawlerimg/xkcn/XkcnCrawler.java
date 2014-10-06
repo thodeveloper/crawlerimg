b6ce01cef432d63ebfe0a8b3a9628154783bde18
tho nguyen <tho.nguyen@tabikobo.com>
2014-10-06 18:02:50 +0700

    Get file name

--- src/main/java/codedi/crawlerimg/xkcn/XkcnCrawler.java
+++ src/main/java/codedi/crawlerimg/xkcn/XkcnCrawler.java
@@ -6,34 +6,36 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
+import org.apache.commons.io.FilenameUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import codedi.crawlerimg.util.CommonUtil;
+public class XkcnCrawler {
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("http://xkcn.info").get();
+		Elements newsHeadlines = doc.select("div.gridItem");
 
-public class XkcnCrawler 
-{
-    public static void main( String[] args ) throws IOException
-    {
-    	Document doc = Jsoup.connect("http://xkcn.info").get();
-    	Elements newsHeadlines = doc.select("div.gridItem");
-    	
-    	for (Element element : newsHeadlines) {
-    		
-    		System.out.println(element.attr("data-photo-high"));
-    		
-    		downloadUsingNIO(element.attr("data-photo-high").toString(), CommonUtil.generateUniqueString("codedi_", ".jpg"));
+		for (Element element : newsHeadlines) {
+
+			System.out.println(element.attr("data-photo-high"));
+			String url = element.attr("data-photo-high");
+			String baseName = FilenameUtils.getBaseName(url);
+			String extension = FilenameUtils.getExtension(url);
+
+			downloadUsingNIO(element.attr("data-photo-high").toString(),
+					baseName + "." + extension);
 		}
-    }
-    
-    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
-        URL url = new URL(urlStr);
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        fos.close();
-        rbc.close();
-    }
+	}
+
+	private static void downloadUsingNIO(String urlStr, String file)
+			throws IOException {
+		URL url = new URL(urlStr);
+		ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		fos.close();
+		rbc.close();
+	}
 }
